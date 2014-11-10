b51e2f23365f7a9cab3e1617bbab862b18179acc
anyonecan <jss087@gmail.com>
2014-11-10 13:54:18 -0600

    added loop thru indexes

--- GraphDemo/Plot.cs
+++ GraphDemo/Plot.cs
@@ -15,10 +15,13 @@ namespace GraphDemo
         {   // comes here when Plot button of demo is clicked
             int indX = xBox.SelectedIndex;
             int indX1 = xBox.SelectedIndex + 1;
+            int indX2 = xBox.SelectedIndex + 2;
+            int indX3 = xBox.SelectedIndex + 3;
             int indY = yBox.SelectedIndex;
             float[,] data = rr.get_Data();
             int nLines = rr.get_nLines();
-            string SeriesCode = "Series";
+            string SeriesCode ;
+            string TmpS;
 
             chart.Series.Clear(); //ensure that the chart is empty
             chart.Legends.Clear(); // moved from below to try to get everything in loop, may have to move back
@@ -30,22 +33,46 @@ namespace GraphDemo
                
            }
  */
-            chart.Series.Add("Series0");
-            chart.Series[0].ChartType = SeriesChartType.Point;
+           for (int i = 0; i < 5 /*Global.nElements */; i++)
+           {
+               TmpS =i.ToString() ;
+               SeriesCode = "Series" + TmpS;
+               chart.Series.Add(SeriesCode);
+               chart.Series[i].ChartType = SeriesChartType.Point; // runs, but now there are lines from the X axis down to the points
 
-            chart.Series.Add("Series1");
-            chart.Series[1].ChartType=SeriesChartType.Point;  // was charttype.line
+           }
 
-            chart.Series.Add("Series2");  // copy pasted this from above
-            chart.Series[2].ChartType = SeriesChartType.Point;  // was charttype.line
+
+           //chart.Series.Add("Series0");   // this was X
+           //chart.Series[0].ChartType = SeriesChartType.Point;
+
+           //chart.Series.Add("Series1");  // this was the first Y
+           //chart.Series[1].ChartType = SeriesChartType.Point;  // was charttype.line
+
+           //chart.Series.Add("Series2");  // copy pasted this from above --- the second X? 
+           //chart.Series[2].ChartType = SeriesChartType.Point;  // was charttype.line
+ 
 
     //       chart.Legends.Clear();
-            for (int j = 0; j < nLines+1; j++)
-            {
-                chart.Series[0].Points.AddXY(data[j, indX], data[j, indY]);
-                chart.Series[1].Points.AddXY(data[j, indX], data[j, indY]);
-                chart.Series[2].Points.AddXY(data[j, indX1], data[j, indY]);  // copy pasted from line above
-            }
+
+            //for (int j = 0; j < nLines+1; j++)
+            //{
+            //    chart.Series[0].Points.AddXY(data[j, indX], data[j, indY]); 
+            // //   chart.Series[1].Points.AddXY(data[j, indX], data[j, indY]);   // same as line above? looks ok w/o
+            //    chart.Series[2].Points.AddXY(data[j, indX1], data[j, indY]);  // copy pasted from line above
+            //    chart.Series[3].Points.AddXY(data[j, indX2], data[j, indY]);  // copy pasted from line above
+            //    chart.Series[4].Points.AddXY(data[j, indX3], data[j, indY]);  // copy pasted from line above
+            //}
+
+           for (int j = 0; j < nLines+1; j++)
+           {
+               for (int indeX = 1; indeX < Global.nElements+1; indeX++)
+               {
+                   chart.Series[j].Points.AddXY(data[j, indeX], data[j, indY]); 
+               }
+           }
+
+
         }
     }
 }
